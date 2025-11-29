9b13d53fb8e6fdacad2dfae29b787216b4f514b4
FABIANCMQ <fab.castmurga@gmail.com>
2025-11-29 12:49:06 -0500

    Agregar archivos de proyecto.

---
+++ CM_Clases/Arista.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM_Clases
+{
+    public class Arista
+    {
+        public Vertice destino;
+        public int costo;
+
+        public Arista sig = null;
+    }
+}
---
+++ CM_Clases/Vertice.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM_Clases
+{
+    public class Vertice
+    {
+        public string dato;
+
+        public Vertice sig = null;
+
+        public Arista lista_Arista = null;
+
+        public void InsertarArista(Vertice d, int c)
+        {
+            Arista nuevo = new Arista();
+            nuevo.destino = d;
+            nuevo.costo = c;
+
+            if (lista_Arista != null)
+            {
+                Arista temp = lista_Arista;
+                while (temp != null)
+                {
+                    temp = temp.sig;
+                }
+                temp.sig = nuevo;
+            }
+            else
+            {
+                lista_Arista = nuevo;
+            }
+        }
+
+        public void MostrarAristas()
+        {
+            Arista temp = lista_Arista;
+            int i = 1;
+            while (temp != null)
+            {
+                Console.WriteLine($"{i}. {temp.destino.dato} ({temp.costo} km)");
+                temp = temp.sig;
+                i++;
+            }
+        }
+    }
+}
